10b715d714fd9553a9796a6ce4b53781424e140f
larswww <lw222ii@student.lnu.se>
2016-09-28 21:38:38 +0800

    fixed a error with single vs double quotes in RegisterView causing TC 4.1 to fail

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -18,25 +18,25 @@ class RegisterView {
     }
 
     public function generateRegisterFormHTML($message) {
-        return "      
+        return '      
 			<h2>Register new user</h2>
-			<form action='?register' method='post' enctype='multipart/form-data'>
+			<form action="?register" method="post" enctype="multipart/form-data">
 				<fieldset>
 				<legend>Register a new user - Write username and password</legend>
-					<p id='" . self::$messageId . "'> $message </p>
-					<label for='" . self::$name . "'>Username :</label>
-					<input type='text' size='20' name='" . self::$name ."' id='" . self::$name ."' value='" . self::$enteredName . "' />
+					<p id="' . self::$messageId . '">' . $message . '</p>
+					<label for="' . self::$name . '">Username :</label>
+					<input type="text" size="20" name="' . self::$name .'" id="' . self::$name .'" value="' . self::$enteredName . '" />
 					<br/>
-					<label for='" . self::$password ."' >Password  :</label>
-					<input type='password' size='20' name='" . self::$password . "' id='" . self::$password ."' value='' />
+					<label for="' . self::$password .'" >Password  :</label>
+					<input type="password" size="20" name="' . self::$password . '" id="' . self::$password .'" value="" />
 					<br/>
-					<label for='" . self::$passwordRepeat ."' >Repeat password  :</label>
-					<input type='password' size='20' name='" . self::$passwordRepeat ."' id='" . self::$passwordRepeat . "' value='' />
+					<label for="' . self::$passwordRepeat .'" >Repeat password  :</label>
+					<input type="password" size="20" name="' . self::$passwordRepeat .'" id="' . self::$passwordRepeat . '" value="" />
 					<br/>
-					<input id='submit' type='submit' name='DoRegistration'  value='Register' />
+					<input id="submit" type="submit" name="DoRegistration"  value="Register" />
 					<br/>
 				</fieldset>
 			</form>
-			 ";
+			 ';
     }
 }
